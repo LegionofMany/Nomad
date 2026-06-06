e874bc6c3478adf498a319131405a937e1a9f3d2
Voltaire <mkolbaford@gmail.com>
2026-06-06 17:13:12 -0600

    Wire travel pocket manager into service container

--- android-nomad/app/src/main/java/protocols/voltaire/nomad/di/NomadServiceContainer.kt
+++ android-nomad/app/src/main/java/protocols/voltaire/nomad/di/NomadServiceContainer.kt
@@ -8,8 +8,10 @@ import protocols.voltaire.nomad.security.InMemorySecureStorageGateway
 import protocols.voltaire.nomad.security.SecureStorageGateway
 import protocols.voltaire.nomad.travel.BasicTravelModeManager
 import protocols.voltaire.nomad.travel.BasicTravelPaymentPolicy
+import protocols.voltaire.nomad.travel.BasicTravelPocketManager
 import protocols.voltaire.nomad.travel.TravelModeManager
 import protocols.voltaire.nomad.travel.TravelPaymentPolicy
+import protocols.voltaire.nomad.travel.TravelPocketManager
 import protocols.voltaire.nomad.ui.NomadHomeController
 import protocols.voltaire.nomad.wallet.DevelopmentWalletEngine
 import protocols.voltaire.nomad.wallet.WalletEngine
@@ -26,6 +28,7 @@ class NomadServiceContainer {
     val clockUnlockManager: ClockUnlockManager = BasicClockUnlockManager(secureStorage)
     val travelModeManager: TravelModeManager = BasicTravelModeManager()
     val travelPaymentPolicy: TravelPaymentPolicy = BasicTravelPaymentPolicy()
+    val travelPocketManager: TravelPocketManager = BasicTravelPocketManager()
     val walletEngine: WalletEngine = DevelopmentWalletEngine()
     val blockpagesSafetyClient: BlockpagesSafetyClient = DevelopmentBlockpagesSafetyClient()
 
