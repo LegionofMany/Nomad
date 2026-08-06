2624e6e51886e02f0541060be291a0365a94136d
Voltaire <mkolbaford@gmail.com>
2026-08-05 19:57:16 -0600

    Export Arkrilium swap adapter

--- mobile/nomad/adapters/index.ts
+++ mobile/nomad/adapters/index.ts
@@ -1,4 +1,5 @@
 export * from './walletAdapter';
 export * from './localNomadAdapters';
+export * from './nomadSwapAdapter';
 export * from './NomadAdaptersProvider';
 export * from './clonedWalletAdapterTemplate';
