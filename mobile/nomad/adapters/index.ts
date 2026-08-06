7b6e978ff497fa89886c3065de1b18a0df5b2aa2
Voltaire <mkolbaford@gmail.com>
2026-08-05 20:16:52 -0600

    Export Nomad security adapter

--- mobile/nomad/adapters/index.ts
+++ mobile/nomad/adapters/index.ts
@@ -2,5 +2,6 @@ export * from './walletAdapter';
 export * from './localNomadAdapters';
 export * from './nomadTravelAdapter';
 export * from './nomadSwapAdapter';
+export * from './nomadSecurityAdapter';
 export * from './NomadAdaptersProvider';
 export * from './clonedWalletAdapterTemplate';
