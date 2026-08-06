e4a94f80ce6e3771ae17659cf07e53613eb63e2e
Voltaire <mkolbaford@gmail.com>
2026-08-05 20:04:09 -0600

    Export Travel Pocket adapter

--- mobile/nomad/adapters/index.ts
+++ mobile/nomad/adapters/index.ts
@@ -1,5 +1,6 @@
 export * from './walletAdapter';
 export * from './localNomadAdapters';
+export * from './nomadTravelAdapter';
 export * from './nomadSwapAdapter';
 export * from './NomadAdaptersProvider';
 export * from './clonedWalletAdapterTemplate';
